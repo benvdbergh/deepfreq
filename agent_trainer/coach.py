5b1259da9dfa0393f202e0c806b3ece3ccaa14c2
benvdbergh <benvdbergh97@gmail.com>
2022-04-20 00:15:39 +0200

    Merge from develop branch

---
+++ agent_trainer/coach.py
@@ -0,0 +1,55 @@
+# Gym stuff
+import gym
+import gym_anytrading
+import random
+
+import tensorflow as tf
+
+from tensorflow.keras.layers import Dense, Flatten, LSTM
+from tensorflow.keras.optimizers import Adam
+
+# Processing libraries
+from matplotlib import pyplot as plt
+from data_manager import importer
+
+from rl.agents import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
+# Initialize data
+df = importer.get_df("user_data\\data\\binance\\BTC_USDT-1m.json")
+
+# Make environment
+env = gym.make('crypto-v0', df=df, frame_bound=(10,df.shape[0]), window_size=10)
+num_timesteps = env.observation_space.shape[0]
+state_size= env.observation_space.shape[1]
+actions = env.action_space.n
+#boopidy boop boop booooop
+
+# Build environment
+state = env.reset()
+
+def build_model(num_timesteps, actions):
+    model = tf.keras.Sequential()
+    #model.add(Flatten(input_shape=(1,num_timesteps,state_size)))
+    model.add(LSTM(5, activation='relu',input_shape=(num_timesteps, state_size)))
+    #model.add(Dense(32, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(actions, activation='linear'))
+    return model
+
+
+model = build_model(num_timesteps, actions)
+model.summary()
+
+
+def build_agent(model, actions):
+    policy = BoltzmannQPolicy()
+    memory = SequentialMemory(limit=50000, window_length=num_timesteps)
+    dqn = DQNAgent(model=model, memory=memory, policy=policy, 
+                  nb_actions=actions, nb_steps_warmup=40, target_model_update=1e-2)
+    return dqn
+  
+dqn = build_agent(model, actions)
+dqn.compile(Adam(learning_rate=1e-3), metrics=['mae'])
+dqn.fit(env, nb_steps=50000, visualize=False, verbose=1, log_interval=10000)
---
+++ agent_trainer/data_manager/importer.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+import numpy as np
+
+
+def get_df(path):
+  df = pd.read_json(path, orient= 'values' )
+  #df = pd.DataFrame(columns=["Date", "Open", "High", "Low", "Close", "Volume"])
+  df.columns = ["date", "open", "high", "low", "close", "volume"]
+  
+
+  df['date'] = pd.to_datetime(df['date'],unit='ms')
+  df.set_index('date', inplace=True)
+  
+  # # check if directory is empty
+  # if not os.path.isdir() or len(os.listdir(path)) == 0:
+  #   print("ERROR Directory does not exist or is empty")
+  #   return
+    
+  # for filename in os.listdir(path):
+  #     f = os.path.join(path, filename)
+      
+  #     # checking if it is a file
+  #     if os.path.isfile(f):
+  #         print(f)
+  return df
+
+if __name__ == "__main__":
+  get_df("C:\\Users\\vandenbb\\source\\vscode\\deepfreq\\user_data\\data\\binance\\BTC_USDT-1m.json")
